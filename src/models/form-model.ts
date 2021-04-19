b60bdc585a2454fee3dfe32d697926cfd0bff975
Apifex <apifex@gmail.com>
2021-04-19 21:33:06 +0200

    add step at position 0 fixed

--- src/models/form-model.ts
+++ src/models/form-model.ts
@@ -1,9 +1,14 @@
 import { Document, Types, Schema, model, Model }from 'mongoose'
+import jwt from 'jsonwebtoken'
   
 export interface IForm {
     name: string;
     steps: [string];
     owner: string;
+    status?: string;
+    clientToken?: string;
+    sheetUrl?: string;
+    sheetId?: string;
     createdOn: Date
   }
 
@@ -11,11 +16,15 @@ interface IFormDocument extends IForm, Document {
     addOwner(): void,
     addStep(stepId: string, position?: number): void,
     editStepsPosition(stepId: string, position?: number): void,
+    generateJWT(): string,
 } 
 
 interface IFormModel extends Model<IFormDocument> {
     build(args: IForm): any
 }
+
+const jwtPrivateSecret = process.env.JWT_PRIVATE_SECRET.replace(/\\n/g, "\n")
+
 const FormSchema = new Schema<IFormDocument, IFormModel>({
     name: {
       type: String,
@@ -27,6 +36,20 @@ const FormSchema = new Schema<IFormDocument, IFormModel>({
     },
     owner: {
       type: String,
+    },
+    clientToken: {
+      type: String, // token for client script
+    },
+    status: {
+      type: String, //created/ published/ closed
+    },
+    sheetUrl: {
+      type: String,
+      default: ''
+    },
+    sheetId: {
+      type: String,
+      default: ''
     },
     createdOn: {
       type: Date,
@@ -40,6 +63,7 @@ FormSchema.methods.addOwner = function (owner: string) {
 
 FormSchema.methods.addStep = function (stepId: string, position?: number) {
   if (!position) position = this.steps.length
+  if (position == 0) position = 0
   this.steps.splice(position, 0, stepId)
 }
 
@@ -49,6 +73,15 @@ FormSchema.methods.editStepsPosition = function(stepId: string, newPosition: num
   this.steps.splice(stepIndex, 1)
   if (newPosition || newPosition == 0) this.steps.splice(newPosition, 0, stepId)
 }
+
+FormSchema.methods.generateJWT = function () {
+  const token = jwt.sign(
+      { _id: this.owner }, 
+      jwtPrivateSecret, 
+      {algorithm: "RS256"})
+  this.clientToken = token
+  return token
+  };
 
 FormSchema.statics.build = (args: IForm) => {
     return new FormModel(args)
