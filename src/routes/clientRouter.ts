01529bbe63c83bdb34c2780c4b0d8cfd6f2afb99
Apifex <apifex@gmail.com>
2021-04-21 21:39:41 +0200

    delete auth from client

--- src/routes/clientRouter.ts
+++ src/routes/clientRouter.ts
@@ -1,14 +1,14 @@
 import express, {NextFunction, Request, Response} from 'express';
 import { saveToSheet } from '../controllers/sheets-controler';
-import passport from '../services/passport/passport-jwt'
 import FormActions from '../controllers/form-controler'
+import passport from '../services/passport/passport-client'
+
+
+const authenticate = passport.authenticate('client', {session: false})
 
 const clientRouter = express.Router();
 
-const authenticate = passport.authenticate('jwt', {session: false})
-//TODO change this authentication
-
-clientRouter.get('/load', authenticate, FormActions.getFullForm) //TODO loadForm
+clientRouter.get('/loadForm', FormActions.loadForm) //TODO loadForm
 
 clientRouter.post('/submit', authenticate, saveToSheet) //submitForm) //TODO
   
